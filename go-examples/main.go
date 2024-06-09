1778ae07c6dc0ead300aed64800c9bd0dd90fc0e
dezhizhang <1541609448@qq.com>
2024-06-09 16:46:04 +0800

    feat:更新

--- go-examples/main.go
+++ go-examples/main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
 	//path, err := filepath.Abs("./")
@@ -12,6 +9,18 @@ func main() {
 
 	//fmt.Println(path, err, "-----")
 
-	path, err := os.Getwd()
-	fmt.Println(path, err)
+	//path, err := os.Getwd()
+	//fmt.Println(path, err)
+
+	//path := os.Args[0]
+	//
+	//fmt.Println(path)
+
+	//ok := true
+
+	//fmt.Printf("%T,%t\n", ok, ok)
+
+	var r rune = 65
+	fmt.Printf("%T,%d\n", r, r)
+
 }
