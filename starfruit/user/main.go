f95774830f9beef8f08c1787bdf1e98ca77d3cab
dezhizhang <1541609448@qq.com>
2024-08-16 21:12:12 +0800

    feat:服务的起停

--- starfruit/user/main.go
+++ starfruit/user/main.go
@@ -1,54 +1,13 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"github.com/gin-gonic/gin"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+	srv "starfruit.top/common"
 )
 
 func main() {
 	r := gin.Default()
 
-	srv := http.Server{
-		Addr:    ":8080",
-		Handler: r,
-	}
-
-	// 优雅启停
-	go func() {
-		log.Printf("start server in %s\n", srv.Addr)
-		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			log.Fatalf("listen: %s\n", err)
-		}
-	}()
-
-	quit := make(chan os.Signal, 1)
-
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-
-	<-quit
-
-	log.Println("shutting down server...")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-
-	defer cancel()
-
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("server forced to shutdown:", err)
-	}
-
-	select {
-	case <-ctx.Done():
-		log.Println("timeout of 2 seconds.")
-	}
-
-	log.Println("server stop successfully!")
+	srv.Run(r, ":8080", "user")
 
 }
