cd88ff81bc6bb364501dd825cd48cbdc7edbd2c6
dezhizhang <1541609448@qq.com>
2024-08-16 22:28:24 +0800

    feat:骗写短信验证码接口

--- starfruit/user/api/user/user.go
+++ starfruit/user/api/user/user.go
@@ -2,15 +2,37 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 	"starfruit.top/common"
+	"starfruit.top/user/pkg/model"
+	"time"
 )
 
 type HandlerUser struct {
 }
 
+// GetCaptcha 获取手机验证码
 func (*HandlerUser) GetCaptcha(ctx *gin.Context) {
 
 	rsp := common.Result{}
+	//1. 获取参数
+	mobile := ctx.PostForm("mobile")
+	//2. 校验参数
+	if common.VerifyMobile(mobile) {
+		ctx.JSON(http.StatusOK, rsp.Fail(model.NoLegalMobil, "手机号不合法"))
+		return
+	}
+	//3. 生成验证码
+	code := "123456"
 
-	ctx.JSON(200, rsp.Success("123456"))
+	//4. 调用短信平台
+	go func() {
+		time.Sleep(2 * time.Second)
+		log.Println("短信平台调用成功，发送短信")
+		//5.存储验证码过期时间为15分钟
+		log.Printf("验证码存入redis成功:REGISTER_%s:%s", mobile, code)
+	}()
+
+	ctx.JSON(200, rsp.Success(code))
 }
---
+++ starfruit/user/pkg/model/code.go
@@ -0,0 +1,7 @@
+package model
+
+import "starfruit.top/common"
+
+const (
+	NoLegalMobil common.BusinessCode = 200 // 手机号不合法
+)
