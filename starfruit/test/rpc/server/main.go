9146a4b6c1604b7748cb7773986469cd919ea502
dezhizhang <1541609448@qq.com>
2024-08-18 21:36:14 +0800

    feat:http服务调用

---
+++ starfruit/test/rpc/server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+		_ = r.ParseForm()
+		fmt.Println("path:", r.URL.Path)
+
+		a, _ := strconv.Atoi(r.Form.Get("a"))
+		b, _ := strconv.Atoi(r.Form.Get("b"))
+		w.Header().Set("content-type", "application/json")
+		_, _ = w.Write([]byte(fmt.Sprintf(`{"a": %d, "b": %d}`, a, b)))
+	})
+	_ = http.ListenAndServe(":8080", nil)
+}
