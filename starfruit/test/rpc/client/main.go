930403beb3658cce52bfd0333839d4ecd75145a8
dezhizhang <1541609448@qq.com>
2024-08-18 22:59:11 +0800

    feat:rpc服务调用

--- starfruit/test/rpc/client/main.go
+++ starfruit/test/rpc/client/main.go
@@ -6,19 +6,15 @@ import (
 )
 
 func main() {
-	// 建立链接
-	conn, err := rpc.Dial("tcp", ":1234")
+	//1.建立连接
+	conn, err := rpc.Dial("tcp", "127.0.0.1:8080")
 	if err != nil {
-		panic("链接失败")
+		panic("连接失败")
 	}
-
 	var reply string
-
 	err = conn.Call("HelloService.Hello", "world", &reply)
 	if err != nil {
-		panic("调用失败")
+		panic("服务调用失败")
 	}
-
 	fmt.Println(reply)
-
 }
--- starfruit/test/rpc/server/main.go
+++ starfruit/test/rpc/server/main.go
@@ -8,17 +8,22 @@ import (
 type HelloService struct{}
 
 func (s *HelloService) Hello(request string, reply *string) error {
-	*reply = "hello " + request
+	*reply = "Hello " + request
 	return nil
 }
 
 func main() {
 	//1. 实例化server
-	listen, _ := net.Listen("tcp", ":1234")
-
-	//2.注册服务名称
-	_ = rpc.RegisterName("HelloService", &HelloService{})
-	//3.启动服务
-	conn, _ := listen.Accept()
+	listen, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	//2. 注册服务
+	err = rpc.RegisterName("HelloService", &HelloService{})
+	if err != nil {
+		panic(err)
+	}
+	// 启动服务
+	conn, err := listen.Accept()
 	rpc.ServeConn(conn)
 }
