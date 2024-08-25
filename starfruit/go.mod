949e45faf934610a7695feabdc9e6dcf4ad4e8b3
dezhizhang <1541609448@qq.com>
2024-08-25 21:44:26 +0800

    feat:服务端错误

--- starfruit/go.mod
+++ starfruit/go.mod
@@ -3,6 +3,7 @@ module starfruit.top
 go 1.22.4
 
 require (
+	github.com/gin-gonic/gin v1.10.0
 	google.golang.org/grpc v1.65.0
 	google.golang.org/protobuf v1.34.2
 	gorm.io/driver/mysql v1.5.7
@@ -16,7 +17,6 @@ require (
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.10.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
@@ -33,8 +33,6 @@ require (
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
-	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/arch v0.9.0 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
--- starfruit/user_web/api/user.go
+++ starfruit/user_web/api/user.go
@@ -1,6 +1,40 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"net/http"
+	"starfruit.top/user_web/proto"
+)
+
+// 将grpc转换成http
+func handlerGrpcErrorToHttp(c *gin.Context, err error) {
+	if err != nil {
+		if e, ok := status.FromError(err); ok {
+			switch e.Code() {
+			case codes.NotFound:
+				c.JSON(http.StatusNotFound, gin.H{
+					"message": e.Message(),
+				})
+				return
+			case codes.Internal:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"message": "服务器出错了",
+				})
+				return
+			default:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"message": "其它错训",
+				})
+
+			}
+
+		}
+	}
+}
 
 // HandleUserLogin 用户登录
 func HandleUserLogin(c *gin.Context) {
@@ -13,7 +47,21 @@ func HandleUserRegister(c *gin.Context) {
 }
 
 // HandleGetUserList 获取用户列表
-func HandleGetUserList(c *gin.Context) {}
+func HandleGetUserList(c *gin.Context) {
+	conn, err := grpc.Dial(":8888", grpc.WithInsecure())
+	handlerGrpcErrorToHttp(c, err)
+
+	defer conn.Close()
+
+	client := proto.NewUserClient(conn)
+	list, err := client.GetUserList(context.Background(), &proto.RequestUser{
+		Page: 1,
+		Size: 20,
+	})
+	handlerGrpcErrorToHttp(c, err)
+
+	c.JSON(200, &list)
+}
 
 // HandleUpdateUser 更新用户信息
 func HandleUpdateUser(c *gin.Context) {
