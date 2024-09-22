27383ef0989a11abb66d3e45c6ae8c6b8ac09cee
dezhizhang <1541609448@qq.com>
2024-09-22 09:43:56 +0800

    feat: update

--- starfruit/user_web/api/user.go
+++ starfruit/user_web/api/user.go
@@ -4,35 +4,10 @@ import (
 	"context"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 	"net/http"
 	"starfruit.top/user_web/proto"
+	"starfruit.top/user_web/utils"
 )
-
-// 将grpc错误转换成http错误
-func handlerGrpcErrorToHttp(c *gin.Context, err error) {
-	if err != nil {
-		if e, ok := status.FromError(err); ok {
-			switch e.Code() {
-			case codes.NotFound:
-				c.JSON(http.StatusNotFound, gin.H{
-					"message": e.Message(),
-				})
-				return
-			case codes.Internal:
-				c.JSON(http.StatusInternalServerError, gin.H{
-					"message": "服务器出错了",
-				})
-				return
-			default:
-				c.JSON(http.StatusInternalServerError, gin.H{
-					"message": "其它错训",
-				})
-			}
-		}
-	}
-}
 
 // HandleUserLogin 用户登录
 func HandleUserLogin(c *gin.Context) {
@@ -47,7 +22,7 @@ func HandleUserRegister(c *gin.Context) {
 // HandleGetUserList 获取用户列表
 func HandleGetUserList(c *gin.Context) {
 	conn, err := grpc.Dial(":8888", grpc.WithInsecure())
-	handlerGrpcErrorToHttp(c, err)
+	utils.HandlerGrpcErrorToHttp(c, err)
 
 	defer conn.Close()
 
@@ -56,12 +31,12 @@ func HandleGetUserList(c *gin.Context) {
 		Page: 1,
 		Size: 20,
 	})
-	handlerGrpcErrorToHttp(c, err)
+	utils.HandlerGrpcErrorToHttp(c, err)
 
 	c.JSON(200, &list)
 }
 
 // HandleUpdateUser 更新用户信息
 func HandleUpdateUser(c *gin.Context) {
-
+	c.JSON(http.StatusOK, gin.H{})
 }
--- starfruit/user_web/utils/error.go
+++ starfruit/user_web/utils/error.go
@@ -1,1 +1,32 @@
 package utils
+
+import (
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"net/http"
+)
+
+// HandlerGrpcErrorToHttp 将grpc错误转换成http错误
+func HandlerGrpcErrorToHttp(c *gin.Context, err error) {
+	if err != nil {
+		if e, ok := status.FromError(err); ok {
+			switch e.Code() {
+			case codes.NotFound:
+				c.JSON(http.StatusNotFound, gin.H{
+					"message": e.Message(),
+				})
+				return
+			case codes.Internal:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"message": "服务器出错了",
+				})
+				return
+			default:
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"message": "其它错训",
+				})
+			}
+		}
+	}
+}
